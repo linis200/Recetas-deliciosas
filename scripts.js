1e2f0a968c6f39a708a9562e31ced73c571adef2
linis200 <85678060+linis200@users.noreply.github.com>
2021-06-10 05:05:02 -0500

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,9 @@
+let menu = document.querySelector("#menu");
+let toggleIicon = document.querySelector("#toggle-icon");
+
+toggleIicon.addEventListener("click",function(){
+    menu.classList.toggle("menu-on");
+});
+
+
+
---
+++ sweetalert.js
@@ -0,0 +1,15 @@
+swal.fire({
+    title:'Bienvenido a RD&F!',
+    text: 'Aprenderas mucho te lo aseguro',
+    icon: 'success',
+    confirmButtonText: '¡Empecemos!',
+    width: '35%',
+    height: '30%',
+    padding: '2rem',
+    background: '#202020',
+    timer: '5000',
+    timerProgressBar: 'true',
+    toast: 'true',
+    position: 'top',
+})
+    
